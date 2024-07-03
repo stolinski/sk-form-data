6c23552abf9d6b136a00addaea377a5202e24b58
Scott Tolinski <scott.tolinski@gmail.com>
2024-07-02 22:10:03 -0600

    whoopsies

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import type { Handle } from "@sveltejs/kit";
 import { parseFormData } from "parse-nested-form-data";
 
 export const form_data: Handle = async ({ event, resolve }) => {
-  const content_type = event.request.headers.get("content-type");
   // Only look for form data
-  if (event.request.method === "POST" && content_type?.includes("form-data")) {
+  const is_action =
+    event.request.headers.get("content-type") === "application/x-www-form-urlencoded";
+
+  if (event.request.method === "POST" && is_action) {
     try {
       // Make a clone to prevent error in already read body
       const request_2 = event.request.clone();
